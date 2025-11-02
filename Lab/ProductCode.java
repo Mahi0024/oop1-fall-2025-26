fbae208fec752c2644f0c634342e444b15a1536f
Mahi0024 <mahibd2004@gmail.com>
2025-11-02 21:27:39 +0600

    25-62770-2

---
+++ Lab/ProductCode.java
@@ -0,0 +1,10 @@
+public class ProductCode {
+    public static void main(String[] args) {
+        String productName = "ASUS";
+        int productId = 6207;
+
+        String productCode = productName + "-"+ productId;
+
+        System.out.println("Product Code: " + productCode);
+    }
+}
