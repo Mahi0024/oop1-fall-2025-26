3f59340f1e3d8baff8f6c4b06d8b846d12f02620
Mahi0024 <mahibd2004@gmail.com>
2025-11-02 21:31:32 +0600

    25-62770-2

--- Converter.java
+++
@@ -1,9 +0,0 @@
-public class Converter {
-    public static void main(String[] args){
-
-        String sentence = String.join("", args);
-
-        System.out.println("Uppercase: " + sentence.toUpperCase());
-        System.out.println("Uppercase: " + sentence.toUpperCase());
-    }
-}
